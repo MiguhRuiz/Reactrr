6b1bdd3500e52b7e4a15be3a0701dbc216e8a688
Miguh Ruiz <miguhruiz@gmail.com>
2017-02-01 21:52:28 +0100

    [Add] Initial Setup: Webpack & Test Component

---
+++ src/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class Test extends React.Component {
+  render() {
+    return (
+      <h1>Reactrr</h1>
+    )
+  }
+}
+
+ReactDOM.render(<Test />, document.getElementById('root'))
+
+export default Test
