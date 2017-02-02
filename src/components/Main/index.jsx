a31a1121e450de9a86e4e0ddc5f73983edc4b11a
Miguh Ruiz <miguhruiz@gmail.com>
2017-02-02 22:09:44 +0100

    [Add] Communication between components

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
 
+import MessagesList from '../MessagesList'
+
 class Main extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      messages: [
+        {
+          text: 'Mensaje del tweet',
+          picture: 'https://pbs.twimg.com/profile_images/825108498040156160/w148h2X_.jpg',
+          displayName: 'Miguh Ruiz',
+          username: 'MiguhRuiz',
+          date: Date.now() - 180000
+        },
+        {
+          text: 'Este es un nuevo mensaje',
+          picture: 'https://pbs.twimg.com/profile_images/825108498040156160/w148h2X_.jpg',
+          displayName: 'Miguh Ruiz',
+          username: 'MiguhRuiz',
+          date: Date.now() - 1800000
+        }
+      ]
+    }
+  }
+
   render() {
     return(
-      <div>Main</div>
+      <MessagesList messages={this.state.messages} />
     )
   }
 }
